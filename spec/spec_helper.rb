90172cf2f4b8d0cdb5bd79abca5f4394cd374c2d
Aidan Feldman <aidan.feldman@gmail.com>
2010-09-16 13:47:24 -0400

    on second thought, no RSpec

--- spec/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'recloner'
-require 'rspec'
-require 'rspec/autorun'
-
-Rspec.configure do |config|
-  c.mock_with :rspec
-end
